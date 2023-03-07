0083c1d4fa0a25b13969737c97dc3822220b0c45
Alston Stone <alstonstone0@gmail.com>
2023-03-07 16:56:52 +0000

    Finished Bank Account

--- src/main/java/BankService.java
+++ src/main/java/BankService.java
@@ -19,27 +19,28 @@ public class BankService {
         this.balance = 0;
     }
     /**
-     * TODO: implement functionality to increase the user's balance by amount.
      * @param amount the amount to be deposited.
      */
     public void deposit(double amount){
+        balance = balance + amount;
 
     }
 
     /**
-     * TODO: implement functionality to decrease the user's balance by an amount.
      * If a withdrawl would result in the user having a negative balance, the withdrawl should not occur.
      * @param amount the amount to be withdrawn.
      */
     public void withdraw(double amount){
-
+        double postWithdraw = balance - amount;
+        if(postWithdraw >= 0.0){
+            balance = postWithdraw;
+        }
     }
 
     /**
-     * TODO: return the balance.
      * @return the user's balance.
      */
     public double getBalance(){
-        return 0;
+        return balance;
     }
 }
